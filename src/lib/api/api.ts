b283d7885add2d6f669a962126f1587d347ae8c2
akashi7 <christiannseko@gmail.com>
2024-06-02 00:05:12 +0200

    url change

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { getFromLocal } from '../../helpers/handleStorage'
 
-const BASE_URL = 'http://localhost:5000/api/'
+const BASE_URL = 'https://todo-api-a64u.onrender.com/api/'
 
 export const baseAPI = createApi({
   baseQuery: fetchBaseQuery({
